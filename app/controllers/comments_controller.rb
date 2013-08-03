09b30f6419c2464afb9b4a6785d5976564808dba
deepti-webonise <deepti@weboniselab.com>
2013-08-04 01:04:02 +0530

    change password completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,14 +5,12 @@ class CommentsController < ApplicationController
   end
 
   def create
-    logger.info "##########################{params.inspect}"
-    #raise error
     @comment = current_user.comments.new(content: params[:content], post_id: params[:post_id])
     if @comment.save
       owner = @comment.post.user
       user = @comment.user
       post_title = @comment.post.title
-      UserMailer.comment_mail(owner, user.first_name, post_title,@comment.content).deliver
+      UserMailer.comment_mail(owner, user.first_name, post_title,@comment.content).deliver if owner.id != user.id
       respond_to do |format|
       end
     else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
 
 
   def edit
+    @user = current_user
   end
 
   def create
@@ -32,6 +33,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
+
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { head :no_content }
